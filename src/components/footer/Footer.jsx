1b083b5ee7f9c66be4c1ad3f78dc6c9edce48222
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-10-25 13:48:34 +0600

    added a database

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,18 +3,17 @@ const Footer = () => {
     <div>
       <footer className="footer p-10 bg-base-300 text-base-content">
         <nav>
-          <header className="footer-title">Services</header>
+          <header className="footer-title">Technology & Electronics</header>
           <a className="link link-hover">Branding</a>
           <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
+          <a className="link link-hover">Sell Product</a>
         </nav>
         <nav>
           <header className="footer-title">Company</header>
           <a className="link link-hover">About us</a>
           <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+          <a className="link link-hover">Product Gallery </a>
+          
         </nav>
         <nav>
           <header className="footer-title">Social</header>
--- src/components/pages/BrandItems.jsx
+++ src/components/pages/BrandItems.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react";
-import {  Link, useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 const BrandItems = () => {
   const { brandName } = useParams();
   const [phoneBrand, setPhoneBrand] = useState([]);
+  
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  console.log(brandName);
+  // console.log(brandName);
   useEffect(() => {
-    fetch("https://technology-and-electronics-server-qpndtqici-priyanka-das-dipa.vercel.app/iPhone")
+    fetch(
+      "https://technology-and-electronics-server-qpndtqici-priyanka-das-dipa.vercel.app/iPhone"
+    )
       .then((res) => res.json())
       .then((data) => {
         const filteredBrand = data.filter(
@@ -26,60 +30,89 @@ const BrandItems = () => {
           key={i}
           type="radio"
           name={`rating_${i}`}
-          className={`mask mask-star ${i <= rating ? 'star-filled' : ''}`}
+          className={`mask mask-star ${i <= rating ? "star-filled" : ""}`}
         />
       );
     }
     return stars;
   };
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === phoneBrand.length - 1 ? 0 : prevIndex + 1
+    );
+  };
 
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? phoneBrand.length - 1 : prevIndex - 1
+    );
+  };
 
   return (
-    <div className="min-h-screen">
-      {phoneBrand.length > 0 ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+    <>
+      <div className="slider-container mb-10">
+        <div className="slider">
           {phoneBrand.map((phone, index) => (
-            <div key={index} className="card w-96 bg-base-100 shadow-xl">
-              <figure className="p-5">
-                <img src={phone.imageUrl} alt="image" />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title text-2xl text-center">{phone.name}</h2>
-                <p className="text-xl font-semibold">
-                  <span className="font-bold">Brand Name: </span> {phone.brandName}
-                </p>
-                <div className="flex justify-start">
-                  <p className="text-lg font-medium">$ {phone.price} TK</p>
-                  <p className="text-lg font-medium">
-                    <span className="font-bold">Type: </span> {phone.type}
-                  </p>
-                </div>
-                <div className="rating">{renderStars(phone.rating)}</div>
-                <div className="card-actions justify-end">
-                  <Link to={`/brand/${phone.brandName}/${phone._id}`}>
-                  
-                  <div className="btn btn-sm">Details</div>
-                  </Link>
-                  <Link  to={`/update/${phone._id}`}>
-                  
-                  <button className="btn btn-sm">Update</button>
-                  </Link>
-                  
-                </div>
-              </div>
+            <div key={index}
+            className={`slide ${index === currentIndex ? "active" : ""}`}
+            >
+              <img
+                src={phone.imageUrl}
+                alt={phone.name}
+                className="slide-image"
+              />
             </div>
           ))}
         </div>
-      ) : (
-        <div className="hero bg-base-200 h-[50vh] text-center py-20">
-          <h1 className="text-2xl font-semibold">Sorry!! There is no Product available.</h1>
-          
-        </div>
-      )}
-    </div>
+        <a onClick={prevSlide} className="btn btn-circle">❮</a> 
+      <a onClick={nextSlide} className="btn btn-circle">❯</a>
+      </div>
+
+      <div className="min-h-screen mt-10">
+        {phoneBrand.length > 0 ? (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+            {phoneBrand.map((phone, index) => (
+              <div key={index}  className="card w-96 bg-base-100 shadow-xl">
+                <figure className="p-5">
+                  <img src={phone.imageUrl} alt="image" />
+                </figure>
+                <div className="card-body">
+                  <h2 className="card-title text-2xl text-center">
+                    {phone.name}
+                  </h2>
+                  <p className="text-xl font-semibold">
+                    <span className="font-bold">Brand Name: </span>{" "}
+                    {phone.brandName}
+                  </p>
+                  <div className="flex justify-start">
+                    <p className="text-lg font-medium">$ {phone.price} TK</p>
+                    <p className="text-lg font-medium">
+                      <span className="font-bold">Type: </span> {phone.type}
+                    </p>
+                  </div>
+                  <div className="rating">{renderStars(phone.rating)}</div>
+                  <div className="card-actions justify-end">
+                    <Link to={`/brand/${phone.brandName}/${phone._id}`}>
+                      <div className="btn btn-sm">Details</div>
+                    </Link>
+                    <Link to={`/update/${phone._id}`}>
+                      <button className="btn btn-sm">Update</button>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="hero bg-base-200 h-[50vh] text-center py-20">
+            <h1 className="text-2xl font-semibold">
+              Sorry!! There is no Product available.
+            </h1>
+          </div>
+        )}
+      </div>
+    </>
   );
 };
-
-
 
 export default BrandItems;
