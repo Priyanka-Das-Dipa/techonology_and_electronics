8c25977d11b23ae13f7d773ceeb9425b1ce7e757
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2024-03-15 23:57:09 +0600

    footer fixed, favicon add

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,60 +1,66 @@
+import { FaFacebook, FaInstagram, FaSkype, FaTwitter } from "react-icons/fa6";
 const Footer = () => {
   return (
-    <div>
-      <footer className="footer p-10 bg-base-300 text-base-content">
+    <footer className=" p-10 bg-black text-white">
+      <div className="max-w-7xl  mx-auto footer border-b-2 pb-5 border-stone-500">
         <nav>
-          <header className="footer-title">Technology & Electronics</header>
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Sell Product</a>
-        </nav>
-        <nav>
-          <header className="footer-title">Company</header>
+          <header className="text-lg font-semibold">Get to Know Us</header>
           <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Product Gallery </a>
-          
+          <a className="link link-hover">Our Services</a>
+          <a className="link link-hover">Stores</a>
+          <a className="link link-hover">Contact us</a>
         </nav>
         <nav>
-          <header className="footer-title">Social</header>
-          <div className="grid grid-flow-col gap-4">
-            <a>
+          <header className="text-lg font-semibold">Customer Services </header>
+          <a className="link link-hover">My Account</a>
+          <a className="link link-hover">Pricing Plans</a>
+          <a className="link link-hover">Terms and Conditions </a>
+          <a className="link link-hover">Our Team </a>
+        </nav>
+        <nav>
+          <header className="text-lg font-semibold">Quick Links</header>
+          <a className="link link-hover">SmartPhones</a>
+          <a className="link link-hover">Laptops & Tablets</a>
+          <a className="link link-hover">Headphones </a>
+          <a className="link link-hover">Gadgets</a>
+        </nav>
+        <nav>
+          <header className="text-lg font-semibold">
+            SignUp to Newsletter
+          </header>
+          <div>
+            <label className="input input-bordered flex items-center gap-2 text-black">
+              <input type="text" className="grow" placeholder="Search" />
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
+                viewBox="0 0 16 16"
+                fill="currentColor"
+                className="w-4 h-4 opacity-70"
               >
-                <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+                <path
+                  fillRule="evenodd"
+                  d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                  clipRule="evenodd"
+                />
               </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-              </svg>
-            </a>
-            <a>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                className="fill-current"
-              >
-                <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-              </svg>
-            </a>
+            </label>
+            <p className="text-xs text-slate-400 pt-2">
+              Stay up to date with all the actions that we are <br />
+              saved for all our customers.
+            </p>
+          </div>
+          <div className="flex gap-5">
+            <FaFacebook  className="text-2xl "/>
+            <FaSkype  className="text-2xl "/>
+            <FaTwitter  className="text-2xl "/>
+            <FaInstagram  className="text-2xl "/>
           </div>
         </nav>
-      </footer>
-    </div>
+      </div>
+      <p className="text-xs text-slate-500 text-center mt-3">
+        © 2024 Technology & Electronics, All Rights Reserved
+      </p>
+    </footer>
   );
 };
 
